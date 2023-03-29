d70c07ff54fd8cc7c96fc07eb58b0a4bc3c55e56
Ethan Pace <ethanpace212@gmail.com>
2023-03-29 20:25:05 +1000

    added get and infrastructure to weather endpoint

--- weather/models.py
+++ weather/models.py
@@ -1,3 +1,33 @@
-from django.db import models
+from json import loads
+import pymongo
 
-# Create your models here.
+client = pymongo.MongoClient("")
+db = client['weatherDataDB']
+coll = db['weather']
+
+def weather(json_object):
+    new_record = {
+        'Time': json_object['Time'],
+        'Device ID': json_object['Device ID'],
+        'Device Name': json_object['Device Name'],
+        'Latitude': json_object['Latitude'],
+        'Longitude': json_object['Longitude'],
+        'Temperature (°C)': json_object['Temperature (°C)'],
+        'Atmospheric Pressure (kPa)': json_object['Atmospheric Pressure (kPa)'],
+        'Lightning Average Distance (km)': json_object['Lightning Average Distance (km)'],
+        'Lightning Strike Count': json_object['Lightning Strike Count'],
+        'Maximum Wind Speed (m/s)': json_object['Maximum Wind Speed (m/s)'],
+        'Precipitation mm/h': json_object['Precipitation mm/h'],
+        'Solar Radiation (W/m2)': json_object['Solar Radiation (W/m2)'],
+        'Vapor Pressure (kPa)': json_object['Vapor Pressure (kPa)'],
+        'Humidity (%)': json_object['Humidity (%)'],
+        'Wind Direction (°)': json_object['Wind Direction (°)'],
+        'Wind Speed (m/s)': json_object['Wind Speed (m/s)']
+    }
+    return new_record
+
+def find(limit):
+    return coll.find().limit(int(limit))
+    
+def search(search_terms):
+    return coll.find_one(search_terms)
--- weather/views.py
+++ weather/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from bson.json_utils import dumps
+import json
+import pymongo
 
-# Create your views here.
+from . import models
+
+def weather(request):
+    if(request.method == "GET"):
+        return get(request)
+    elif(request.method == "POST"):
+        return post(request)
+    elif(request.method == "PUT"):
+        return put(request)
+    elif(request.method == "DELETE"):
+        return delete(request)
+    
+def get(request):
+    body = request.body.decode('utf-8')
+    if(len(body) > 0):
+        json_data = json.loads(body)
+        try:
+            cursor = models.search(json_data['search_terms'])
+        except:
+            cursor = models.find(json_data['limit'])
+    else:
+        cursor = models.find(10)
+    cursor_list = list(cursor)
+    json_data = dumps(cursor_list)
+    return JsonResponse(json_data)
+
+def post(request):
+    return HttpResponse()
+def put(request):
+    return HttpResponse()
+def delete(request):
+    return HttpResponse()
