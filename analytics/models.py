ffe94c37f8c8e7973593cad9ec5e69571672f182
Ethan Pace <ethanpace212@gmail.com>
2023-04-02 10:40:45 +1000

    Added code to analytics (untested, nice to have)

--- analytics/models.py
+++ analytics/models.py
@@ -1,3 +1,28 @@
-from django.db import models
+from json import loads
+import pymongo
 
-# Create your models here.
+#Analytics:
+#		- Tasks:
+#			- Find the maximum precipitation recorded in the last 5 years (single)			
+#		- Methods: 
+#			- Get
+#				- Input JSON: {field/column, aggregation}
+#				- Output JSON {the aggregated data on the chosen column}
+
+client = pymongo.MongoClient("mongodb+srv://testUser:testPassword@nasadata.dpq7x0s.mongodb.net/test")
+db = client['weatherDataDB']
+coll = db['weatherData']
+
+#Get_aggregation
+#Returns the aggregated data on the chosen column
+#Parameters: field, aggregation
+def get_aggregation(field, aggregation):
+    #Aggregation pipeline
+    pipeline = [
+        #Filter out data before 2015
+        { "$match": { "Date": { "$gte": "2015-01-01" } } },
+        #Group by the aggregation
+        { "$group": { "_id": None, "max_precipitation": { aggregation: "$" + field } } }
+    ]
+    #Return the aggregated data
+    return coll.aggregate(pipeline) #Returns a cursor
--- analytics/views.py
+++ analytics/views.py
@@ -1,4 +1,22 @@
-from django.shortcuts import render
-
+#Imports
+from django.http import HttpResponse, JsonResponse
+import json
+from bson.json_util import dumps
+import pymongo
+from . import models
+#Analytics View
+#Splits the request into the appropriate methods
 def analytics(request):
-    return
+    if(request.method == "GET"):
+        return get(request)
+#Get
+#Returns the aggregated data on the chosen column
+#Parameters: field, aggregation
+def get(request):
+    body = request.body.decode('utf-8')
+    try:
+        json_data = json.loads(body)
+        response = models.get_aggregation(json_data['field'], json_data['aggregation'])
+    except:
+        return HttpResponse("body must be json with field and aggregation")
+    return JsonResponse(dumps(response))
