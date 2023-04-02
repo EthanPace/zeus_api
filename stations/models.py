e1cdd9ad1b1fcc12dae9cbae0ded8ec5fc5f87fc
Ethan Pace <ethanpace212@gmail.com>
2023-04-02 10:34:27 +1000

    Added code to stations app (again, untested)

--- stations/models.py
+++ stations/models.py
@@ -13,30 +13,18 @@ import pymongo
 # 				- Input JSON: {weather station & new values}
 # 				- Output JSON: {success / fail}
 # 		- Triggers:
-# 			- If there are not enough weather stations per state, add dummy data		 
+# 			- If there are not enough weather station per state, add dummy data		 
 
 client = pymongo.MongoClient("mongodb+srv://testUser:testPassword@nasadata.dpq7x0s.mongodb.net/test")
 db = client['weatherDataDB']
-coll = db['NSWWeatherData']
+coll = db['stations']
 
-def stations(json_object):
+def station(json_object):
     new_record = {
-        'Time': json_object['Time'],
         'Device ID': json_object['Device ID'],
         'Device Name': json_object['Device Name'],
         'Latitude': json_object['Latitude'],
         'Longitude': json_object['Longitude'],
-        'Temperature (°C)': json_object['Temperature (°C)'],
-        'Atmospheric Pressure (kPa)': json_object['Atmospheric Pressure (kPa)'],
-        'Lightning Average Distance (km)': json_object['Lightning Average Distance (km)'],
-        'Lightning Strike Count': json_object['Lightning Strike Count'],
-        'Maximum Wind Speed (m/s)': json_object['Maximum Wind Speed (m/s)'],
-        'Precipitation mm/h': json_object['Precipitation mm/h'],
-        'Solar Radiation (W/m2)': json_object['Solar Radiation (W/m2)'],
-        'Vapor Pressure (kPa)': json_object['Vapor Pressure (kPa)'],
-        'Humidity (%)': json_object['Humidity (%)'],
-        'Wind Direction (°)': json_object['Wind Direction (°)'],
-        'Wind Speed (m/s)': json_object['Wind Speed (m/s)']
     }
     return new_record
 
@@ -47,16 +35,10 @@ def search(search_terms):
     return coll.find_one(search_terms)
 
 def create(new):
-    return coll.insert_one(stations(new))
+    return coll.insert_one(station(new))
 
-def bulk_create(new_array):
-    object_list = []
-    for new in new_array:
-        object_list += stations(new)
-    return coll.insert_many(object_list)
+def update(search_terms, new):
+    return coll.update_one(search_terms, new)
 
 def delete(search_terms):
     return coll.delete_one(search_terms)
-
-def bulk_delete(search_terms):
-    return coll.delete_many(search_terms)
--- stations/views.py
+++ stations/views.py
@@ -1,56 +1,66 @@
+#Imports
 from django.http import HttpResponse, JsonResponse
 import json
 from bson.json_util import dumps
 import pymongo
-
 from . import models
-
+#Stations View
+#Splits the request into the appropriate methods
 def stations(request):
-    if(request.method == "GET"):
-        return get(request)
-    elif(request.method == "POST"):
+    if(request.method == "POST"):
         return post(request)
     elif(request.method == "PUT"):
         return put(request)
     elif(request.method == "DELETE"):
         return delete(request)
-    
-def get(request):
-    body = request.body.decode('utf-8')
-    if(len(body) > 0):
-        print("long")
-        json_data = json.loads(body)
-        try:
-            cursor = stations.search(json_data['search_terms'])
-        except:
-            cursor = stations.find(json_data['limit'])
-    else:
-        print("short")
-        cursor = stations.find(10)
-    cursor_list = list(cursor)
-    json_data = dumps(cursor_list)
-    return JsonResponse(json_data, safe=False)
-
+#Post
+#Creates a new station or stations
+#Parameters: new (record/array), bulk (boolean)
 def post(request):
     body = request.body.decode('utf-8')
-    json_data = json.loads(body)
-    if(json_data['bulk'] == "false"):
-        response = stations.create(json_data)
-    elif(json_data['bulk'] == "true"):
-        response = stations.bulk_create(json_data)
-    return HttpResponse()
-
+    try:
+        json_data = json.loads(body)
+        if 'bulk' in json_data:
+            if json_data['bulk'] == "false":
+                response = models.create(json_data['new'])
+            elif json_data['bulk'] == "true":
+                response = models.bulk_create(json_data['new'])
+        else:
+            response = models.create(json_data['new'])
+    except:
+        return HttpResponse("false")
+    return HttpResponse(response)
+#Put
+#Updates a station or stations
+#Parameters: search_terms, new (record/array), bulk (boolean)
 def put(request):
-    return HttpResponse()
-
+    body = request.body.decode('utf-8')
+    try:
+        json_data = json.loads(body)
+        if 'bulk' in json_data:
+            if json_data['bulk'] == "false":
+                response = models.update(json_data['search_terms'], json_data['new'])
+            elif json_data['bulk'] == "true":
+                response = models.bulk_update(json_data['search_terms'], json_data['new'])
+        else:
+            response = models.update(json_data['search_terms'], json_data['new'])
+    except:
+        return HttpResponse("false")
+    return HttpResponse(response)
+#Delete
+#Deletes a station or stations
+#Parameters: search_terms, bulk (boolean)
 def delete(request):
     body = request.body.decode('utf-8')
     try:
         json_data = json.loads(body)
+        if 'bulk' in json_data:
+            if json_data['bulk'] == "false":
+                response = models.delete(json_data['search_terms'])
+            elif json_data['bulk'] == "true":
+                response = models.bulk_delete(json_data['search_terms'])
+        else:
+            response = models.delete(json_data['search_terms'])
     except:
         return HttpResponse("false")
-    if(json_data['bulk'] == "false"):
-        response = stations.delete(json_data)
-    elif(json_data['bulk'] == "true"):
-        response = stations.bulk_delete(json_data)
     return HttpResponse(response)
