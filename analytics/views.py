f4a956b16778d18dd618a384c4467cbd1b2e30ac
Ethan Pace <ethanpace212@gmail.com>
2023-04-05 11:03:27 +1000

    changed analytics get from body to query

--- analytics/views.py
+++ analytics/views.py
@@ -13,10 +13,9 @@ def analytics(request):
 #Returns the aggregated data on the chosen column
 #Parameters: field, aggregation
 def get(request):
-    body = request.body.decode('utf-8')
     try:
-        json_data = json.loads(body)
+        json_data = request.GET
         response = models.get_aggregation(json_data['field'], json_data['aggregation'])
     except:
         return HttpResponse("body must be json with field and aggregation")
-    return JsonResponse(dumps(response))
+    return JsonResponse(dumps(list(response)), safe=False)
