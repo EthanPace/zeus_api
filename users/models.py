e4462eac77e852d1cc27e48957f9a2c34250bc39
Ethan Pace <ethanpace212@gmail.com>
2023-04-11 17:25:56 +1000

    Added comments to user/model

--- users/models.py
+++ users/models.py
@@ -4,36 +4,37 @@ import pymongo
 
 # Users:
 # 		- Tasks:
-# 			- Insert a new user (single)
-# 			- Delete a user (single)
-# 			- Delete multiple users (multiple)
-# 			- Update access level for at least two users in the same query (multiple)
+# 			- Insert a new user (single) -- DONE
+# 			- Delete a user (single) -- UNSURE
+# 			- Delete multiple users (multiple) -- TO DO (BROKEN)
+# 			- Update access level for at least two users in the same query (multiple) -- UNSURE
 			
 # 		- Methods: 			
-# 			- Post:			
-# 				- Input JSON: {1 new user object}
-# 				- Output JSON: {success / fail}
+# 			- Post:
+# 				- Input JSON: {1 new user object} -- DONE
+# 				- Output JSON: {success / fail} -- DONE
 
-# 				- Input JSON: {Multiple new user object}
-# 				- Output JSON: {success / fail}
+# 				- Input JSON: {Multiple new user object} -- DONE
+# 				- Output JSON: {success / fail} -- DONE
 				
 			
 # 			- PUT:
-# 				- Input JSON: {new access levels for at least 2 users (in the same query)}
-# 				- Output JSON: {success / fail}
+# 				- Input JSON: {new access levels for at least 2 users (in the same query)} -- UNSURE
+# 				- Output JSON: {success / fail} -- UNSURE
 # 			- DELETE:
-# 				- Input JSON: {Single user}
+# 				- Input JSON: {Single user} -- UNSURE
 					
-# 				- Input JSON: {Multiple users)
+# 				- Input JSON: {Multiple users) -- TO DO (BROKEN)
+
 # 		- Trigger:
-# 			- Configure last logged in trigger in atlas 	
-#           - Assign role to public if isn't admin or manager?		
+# 			- Configure last logged in trigger in atlas -- TO DO
+#           - Assign role to public if isn't admin or manager?	-- DONE	
 # 		- Secondary endpoint:
 # 			- Authenticate	
 # 				- Methods: 			
-# 					- GET:
-# 						- Input JSON: {username + password}
-# 						- Output JSON: {if authenticated}}
+# 					- GET: 
+# 						- Input JSON: {username + password} -- DONE
+# 						- Output JSON: {if authenticated}} -- DONE
 
 # Create your models here.
 client = pymongo.MongoClient("mongodb+srv://testUser:testPassword@nasadata.dpq7x0s.mongodb.net/test")
