204a29c759fde1b5aafff4549b3a14ad8c83d0dc
Hamish <hstockwell151@gmail.com>
2023-04-16 18:07:37 +1000

    Added CORS, not currently working properly, even when I commented out the 'ALLOWED_HOSTS' and the whitelist. I will look into this later

--- zeus_api/settings.py
+++ zeus_api/settings.py
@@ -27,8 +27,22 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-CORS_ORIGIN_ALLOW_ALL = True
+'''
+CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3030',
+] # If this is used, then not need to use `CORS_ALLOW_ALL_ORIGINS = True`
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    'http://localhost:3030',
+]
+'''
 
+CORS_ORIGIN_ALLOW_ALL = False
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3030',
+]
 # Application definition
 
 INSTALLED_APPS = [
